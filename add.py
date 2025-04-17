1f303455b1ee2b7267ea104f4a839d2b12cc957e
Vedant Mishra <68152592+Ardenbruh@users.noreply.github.com>
2025-04-17 14:24:11 +0530

    Update and rename add.py to weather.py

--- add.py
+++
@@ -1,7 +0,0 @@
-# sub_module.py
-
-def subtract(a, b):
-    return a - b
-
-if __name__ == "__main__":
-    print("Subtract 10 - 4 =", subtract(10, 4))  # Output: 6
---
+++ weather.py
@@ -0,0 +1,11 @@
+# weather_app.py
+
+def get_temperature():
+    return "Temperature: 24°C"
+
+def get_forecast():
+    return "Forecast: Clear skies"
+
+if __name__ == "__main__":
+    print(get_temperature())
+    print(get_forecast())
